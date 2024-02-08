9d600524cb16ca564c64bf7e4d79100a9ab156c5
davhasratyan <davhasratan@gmail.com>
2024-02-08 17:26:56 +0400

    photocard

--- resources/views/mainPage/components/photoCard.blade.php
+++ resources/views/mainPage/components/photoCard.blade.php
@@ -6,7 +6,7 @@
 {{--</div>--}}
 
 <div class="card my-4">
-    <img class="card-img-top" src="{{asset('storage/images/mainImage/' . $photo->main_image)}}" alt="Card image cap">
+    <img class="card-img-top photo-card-image" src="{{asset('storage/images/mainImage/' . $photo->main_image)}}" alt="Card image cap">
     <div class="card-body photo-card-body">
         <h5 class="card-title photo-card-title">{{ Str::limit($photo->title, 110, '...') }}</h5>
     </div>

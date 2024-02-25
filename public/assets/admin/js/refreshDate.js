f948b4101440f64ecffaecd297d260b613e78bb4
davhasratyan <davhasratan@gmail.com>
2024-02-25 16:25:15 +0400

    format

--- public/assets/admin/js/refreshDate.js
+++ public/assets/admin/js/refreshDate.js
@@ -1,11 +1,10 @@
-    const dateInput = document.getElementById('dateInput');
-    const refreshButton = document.getElementById('refreshDate');
+const refreshButton = document.getElementById('refreshDate');
+const dateInput = document.getElementById('dateInput');
 
-    refreshButton.addEventListener('click', function () {
-        const now = new Date();
-        const formattedDate = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')} ${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
-        dateInput.value = formattedDate;
-    });
-
+refreshButton.addEventListener('click', function () {
+    const now = new Date();
+    const formattedDate = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')} ${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
+    dateInput.value = formattedDate;
+});
 
 
